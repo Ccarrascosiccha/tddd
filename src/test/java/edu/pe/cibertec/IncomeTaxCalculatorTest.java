056af5154b5dd5abf65bee578df14769af63e41e
Ccarrasco <ccarrascosiccha@gmail.com>
2026-03-31 03:18:29 -0500

    Tax until 7 to 12 UIT

---
+++ src/test/java/edu/pe/cibertec/IncomeTaxCalculatorTest.java
@@ -0,0 +1,58 @@
+package edu.pe.cibertec;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+@DisplayName("IncomeTaxCalculator - Scaled Tax Brackets")
+public class IncomeTaxCalculatorTest {
+
+    private IncomeTaxCalculator calculator;
+    private static double UIT = 4_950.0;
+    private static final double DELTA = 0.01;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new IncomeTaxCalculator();
+    }
+
+    @Test
+    @DisplayName("Income below 7 UIT pays 0% tax")
+    void givenIncomeBelowSevenUIT_whenCalculate_thenZeroTax() {
+        double income = 6 * UIT;
+        assertEquals(0.0, calculator.calculate(income), DELTA);
+    }
+
+    @Test
+    @DisplayName("Income exactly at 7 UIT pays 0% tax")
+    void givenIncomeExactlySevenUIT_whenCalculate_thenZeroTax() {
+        double income = 7 * UIT;
+        assertEquals(0.0, calculator.calculate(income), DELTA);
+    }
+
+    @Test
+    @DisplayName("Income within bracket 7 to 12 UIT pay 8% on excess")
+    void givenIncomeInBracketSevenToTwelveUIT_whenCalculate_thenRightPercent() {
+        double income = 10 * UIT;
+        double expected = (10 - 7) * UIT * 0.08;
+        assertEquals(expected, calculator.calculate(income), DELTA);
+    }
+    @Test
+    @DisplayName("Income within bracket 7 to 12 UIT pay 8% on excess")
+    void giveIncomeInBracketSevenToTwelveUIT_whenCalculate_thenEightPercent() {
+        double income = 10 * UIT;
+        double expected = (10 - 7) * UIT * 0.08;
+        assertEquals(expected, calculator.calculate(income), DELTA);
+    }
+    @Test
+    @DisplayName("Income within bracket 12 to 27 UIT pay 8% on excess")
+    void giveIncomeInBracketTwelveToTwentySevenUIT_whenCalculate_thenAccumulateTax() {
+        double income = 20 * UIT;
+        double expected = (5 * UIT * 0.08) + (8 * UIT * 0.14);
+        assertEquals(expected, calculator.calculate(income), DELTA);
+    }
+
+}
